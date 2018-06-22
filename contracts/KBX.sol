78bfb6cbc72b6fa560c422ca343599ebb09d651b
Subramanian Venkatesan <subramanian.bsv@gmail.com>
2018-06-22 22:42:47 +0530

    remove unwanted comment

--- contracts/KBX.sol
+++ contracts/KBX.sol
@@ -58,8 +58,6 @@ contract SimpleToken is StandardToken, BurnableToken, Whitelist {
 
 
   function setReleaseAgent(address addr) onlyOwner inReleaseState(false) public {
-
-    // We don't do interface check here as we might want to a normal wallet address to act as a release agent
     releaseAgent = addr;
   }
 
